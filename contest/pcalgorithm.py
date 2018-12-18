162df57c8b971a216a0f3d4912f752a3b7200e10
alexfoglia1 <alex.f1793@gmail.com>
2018-12-18 18:15:56 +0100

    presentazione finita

--- contest/pcalgorithm.py
+++ contest/pcalgorithm.py
@@ -164,69 +164,31 @@ def test_butterfly_model():
 def from_file(filename, separator = ","):
     alpha = .05
     dataset = pd.read_csv(filename,sep = separator)
-    (g,sep_set) = get_skeleton(dataset, alpha,dataset.columns)
-    print(g)
-    plot(g,dataset.columns)
+    (g,sep_set) = get_skeleton(dataset, alpha)
     g = to_cpdag(g,sep_set)
     print(g)
     plot(g,dataset.columns)
 
 def gen_lin_reg_model(a,b,N):
-    alpha = .05
-    import time
-    t0 = time.time()
+    alpha = .10
     x1 = rnorm(N)
     x2 = a*x1+rnorm(N)
     x3 = a*x1+rnorm(N)
     x4 = a*x1+rnorm(N)
     y = b*x2 + b*x3 + b*x4 + rnorm(N) + 5
-    tf = time.time()
-    delta_t0 = (tf-t0)
     dataset = pd.DataFrame({'x1': x1.tolist(),
                        'x2': x2.tolist(),
                        'x3': x3.tolist(),
                        'x4': x4.tolist(),
                         'y': y.tolist()
                         })
-    t0 = time.time()
-    (g,sep_set) = get_skeleton(dataset, alpha,dataset.columns)
-    tf = time.time()
-    delta_t1 = (tf-t0)
-    print(g)
-    #plot(g,dataset.columns)
-    t0 = time.time()
-    g = to_cpdag(g,sep_set)
-    tf = time.time()
-    print(g)
-    delta_t2 = (tf-t0)
-    #plot(g,dataset.columns)
-    return (delta_t0,delta_t1,delta_t2)
-
-def gen_lrg2():
-    alpha = .05
-    N = 99999
-    x1 = rnorm(N)
-    x2 = 3*x1+rnorm(N)
-    y = 3*x2+rnorm(N)
-    dataset = pd.DataFrame({
-                       'x1': x1.tolist(),
-                       'x2': x2.tolist(),
-                        'y': y.tolist()
-                        })
-    (g,sep_set) = get_skeleton(dataset,alpha,dataset.columns)
+    (g,sep_set) = get_skeleton(dataset, alpha)
     g = to_cpdag(g,sep_set)
     plot(g,dataset.columns)
-
-def test_linear_regression():
-    (t0,t1,t2) = gen_lin_reg_model(3,5,999999)
-    print "Time to generate: "+str(t0)+" sec"
-    print "Time to estimate skeleton: "+str(t1)+" sec"
-    print "Time to estimate dag: "+str(t2)+" sec"
-    print "Total time: "+str(t1+t0+t2)+" sec"
     
 if __name__ == '__main__':
-    test_butterfly_model()
-    #from_file('marks.dat',separator = ',')
+    #gen_lin_reg_model(3,5,99999)
+    from_file("software.csv")
 
 
     
